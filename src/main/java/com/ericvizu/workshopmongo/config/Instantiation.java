42a892407363e7302d960ab7018ff79949b76867
Eric Vizu <eric_vizu@hotmail.com>
2023-12-14 14:29:13 -0300

    Referenciando os posts do usuário e correção da instantiation

--- src/main/java/com/ericvizu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ericvizu/workshopmongo/config/Instantiation.java
@@ -45,8 +45,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt1), "Bom dia", "Acordei feliz hoje!",
 				new AuthorDTO(maria));
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
